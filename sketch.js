a32177b5ceef847535d748e7446663b27cc9210b
rabovskybrandon <BrandonRabovsky@Suzannes-MacBook-Pro-2.local>
2017-05-18 11:53:06 -0400

    haxball phone 1
    first step

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var xValue;
+var yValue;
+var zValue;
+function setup(){
+createCanvas()
+ createCanvas(800,600);
+}
+
+function draw(){
+  background(255,255,255)
+text("X: "+rotationX,100,100);
+text("Y: "+rotationY,100,200);
+text("Z: " +rotationZ,100,300);
+
+}
